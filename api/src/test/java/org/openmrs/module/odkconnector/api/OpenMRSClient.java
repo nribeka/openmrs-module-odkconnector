452dba8280cfb63464bb7dbb31751061b15a39f8
nribeka <nyoman.ribeka@gmail.com>
2012-05-23 12:27:36 +0300

    odkconnector: removing idea configuration files

---
+++ api/src/test/java/org/openmrs/module/odkconnector/api/OpenMRSClient.java
@@ -0,0 +1,11 @@
+package org.openmrs.module.odkconnector.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nribeka
+ * Date: 5/22/12
+ * Time: 12:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpenMRSClient {
+}
